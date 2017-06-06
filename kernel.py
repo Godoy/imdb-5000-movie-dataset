dff9c098f5e2c53931e31e8ae24920578d94ab29
Adriano Godoy <godoy.ccp@gmail.com>
2017-06-05 22:28:57 -0300

    Plots a correlation map

--- kernel.py
+++ kernel.py
@@ -3,6 +3,9 @@
 
 #import numpy as np 
 import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 movies = pd.read_csv('data/movie_metadata.csv')
 
@@ -19,3 +22,7 @@ movies.color = movies.color.map({'Color': 1, ' Black and White':0})
 print(movies.color.describe());
 
 
+f, ax = plt.subplots(figsize=(12, 10))
+plt.title('Correlation of Movie Features')
+# Draw the heatmap using seaborn
+sns.heatmap(movies.corr(),linewidths=0.1, square=True, cmap="YlGnBu", linecolor='black', annot=True)
