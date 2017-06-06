e1298358fb1d9f2259786b3ff690335efbb63b76
Adriano Godoy <godoy.ccp@gmail.com>
2017-06-05 22:25:11 -0300

    Clean color attribute filling with moda

--- kernel.py
+++ kernel.py
@@ -4,7 +4,18 @@
 #import numpy as np 
 import pandas as pd 
 
-
 movies = pd.read_csv('data/movie_metadata.csv')
 
-print(movies.describe());
+
+#print(movies.info())
+#print(movies.color.describe());
+#print(movies.color.mode()[0]);
+#print(movies.dtypes)
+
+movies.color = movies.color.fillna(movies.color.mode()[0])
+movies.color = movies.color.map({'Color': 1, ' Black and White':0})
+
+
+print(movies.color.describe());
+
+
